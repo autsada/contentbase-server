e8d20fe4ef4c61215bd82d49094ecdf2709345cb
Autsada <autsada.t@gmail.com>
2022-09-17 19:00:00 +0700

    feat:update firebase admin to use default credentials

--- src/lib/config/firebase.ts
+++ src/lib/config/firebase.ts
@@ -1,19 +1,17 @@
 import admin from 'firebase-admin'
-import { initializeApp, getApps, getApp } from 'firebase-admin/app'
+import {
+  initializeApp,
+  getApps,
+  getApp,
+  applicationDefault,
+} from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 import { getAuth } from 'firebase-admin/auth'
-
-const { FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY, FIREBASE_CLIENT_EMAIL } =
-  process.env
 
 function initializeFirebaseAdmin() {
   return !getApps.length
     ? initializeApp({
-        credential: admin.credential.cert({
-          projectId: FIREBASE_PROJECT_ID,
-          privateKey: FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-          clientEmail: FIREBASE_CLIENT_EMAIL,
-        }),
+        credential: applicationDefault(),
       })
     : getApp()
 }
